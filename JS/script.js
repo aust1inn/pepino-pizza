9a80f1b9b0815e70baee1e67c94f6c3a86ae3a16
aust1inn <austinotieno364@gmail.com>
2022-03-17 19:00:18 +0300

    appends values in my orders list

--- JS/script.js
+++ JS/script.js
@@ -8,16 +8,48 @@ function Pizza (size ,crust) {
 
 $(document).ready(function(){
 
-   let pizzaSize= $("#pizza-size").val();
-   let pizzaCrust= $("#crust").val();
-   let pizzaToppings = new Array ();
-
-   $('input[name="toppings"]:checked').each(function() {
-    pizzaToppings.push(this.value);
-    });
-
     $("#order-btn").click(function(){
-        alert(pizzaToppings)
+
+
+        let pizzaSize= $("#pizza-size").val();
+        let pizzaCrust= $("#crust").val();
+        let pizzaToppings = new Array ();
+        
+        $('input[name="toppings"]:checked').each(function() {
+            pizzaToppings.push(this.value);
+            });
+
+        let myPizza = new Pizza (pizzaSize ,pizzaCrust)
+        myPizza.toppings=pizzaToppings
+
+        // size pricing
+        //     let sizePrice = 0
+        // if (myPizza.size=small) {
+        //     let sizePrice = 300
+        // }
+        // else if (myPizza.size=medium) {
+        //     let sizePrice = 600
+        // }
+        // else if (myPizza.size=large) {
+        //     let sizePrice = 900
+        // }
+
+        $("#my-orders").append(
+            "<tr>" +
+            "<td>" +
+            myPizza.size +
+            "</td>" +
+            "<td>" +
+            myPizza.crust +
+            "</td>" +
+            "<td>" +
+            myPizza.toppings +
+            "</td>"+
+            // "<td>"+
+            // sizePrice +
+            // "</td>" +
+            "</tr>"
+        );
     });
 });
 
