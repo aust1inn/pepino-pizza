2e220f279aa71bf622b2bd9943aa17834697b3fc
aust1inn <austinotieno364@gmail.com>
2022-03-17 17:44:34 +0300

    picks the values of toppings, crust and size

--- JS/script.js
+++ JS/script.js
@@ -4,3 +4,20 @@ function Pizza (size ,crust) {
     this.toppings = []
 };
 
+// user logic
+
+$(document).ready(function(){
+
+   let pizzaSize= $("#pizza-size").val();
+   let pizzaCrust= $("#crust").val();
+   let pizzaToppings = new Array ();
+
+   $('input[name="toppings"]:checked').each(function() {
+    pizzaToppings.push(this.value);
+    });
+
+    $("#order-btn").click(function(){
+        alert(pizzaToppings)
+    });
+});
+
